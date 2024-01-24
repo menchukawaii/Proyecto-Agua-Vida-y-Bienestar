bf2f1d00b0f6e6790d1e314255240ddc82655aa1
Carmen <carmensitagarr@gmail.com>
2024-01-24 14:48:39 +0100

    init css

---
+++ php/login.php
@@ -0,0 +1,54 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $user = $_POST["usuario"];
+    $password = $_POST["passwd"];
+
+
+    $dsn = "mysql:dbname=ecohuerto;host=127.0.0.1;port=3307";
+    $usuario = "root";
+    $clave = "";
+    try {
+        $bd = new PDO($dsn, $usuario, $clave);
+        $sql = "SELECT * from usuarios u where u.usuario=\"$user\" and u.passw=\"$password\"; ";
+        $resultado = $bd->query($sql);
+        $count = $resultado->rowCount();
+        if ($count == 1) {
+            echo "Usuario conectado";
+        } else {
+            echo "Error de búsqueda";
+        }
+        $bd = null;
+    } catch (PDOException $e) {
+        echo "Falló la conexión: " . $e->getMessage();
+    }
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/login.css">
+    <title>Inicio Sesion</title>
+</head>
+
+<body>
+    <section>
+        <h1>Iniciar Sesión</h1>
+        <form action="login.php" method="post">
+            <label>Usuario:
+                <input type="text" name="usuario" placeholder="Nombre de usuario">
+            </label>
+            <label>Contraseña:
+                <input type="password" name="passwd" placeholder="Contraseña">
+            </label>
+            <button>Iniciar Sesión</button>
+        </form>
+    </section>
+
+</body>
+
+</html>
---
+++ php/signup.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro</title>
+</head>
+
+<body>
+    <h1>Registro Usuario</h1>
+    <form action="signup.php" method="post">
+        <label>Nombre:
+            <input type="text" name="nombre">
+        </label>
+        <label>Apellidos:
+            <input type="text" name="apellidos">
+        </label>
+        <label>Correo:
+            <input type="email" name="correo">
+        </label>
+        <label>Usuario:
+            <input type="text" name="usuario">
+        </label>
+        <label>Contraseña:
+            <input type="password" name="passwd">
+        </label>
+        <label>Confirmar contraseña:
+            <input name="confirmpasswd" type="password">
+        </label>
+        <button>Enviar</button>
+    </form>
+
+
+
+</body>
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $dsn = "mysql:dbname=ecohuerto;host=127.0.0.1;port=3307";
+    $usuario = "root";
+    $clave = "";
+    $camposValidos = true;
+    $nombre = $_POST["nombre"];
+    $patron = "/^[a-z]{3,n}$/";
+    if (preg_match($patron, $nombre)) {
+        echo "La cadena $nombre es valida";
+    } else {
+        echo "La cadena $nombre no es valida";
+    }
+    $apellidos = $_POST["apellidos"];
+
+    $patron = "/^[a-z]{3,n}$/";
+    if (preg_match($patron, $apellidos)) {
+        echo "La cadena $apellidos es valida";
+    } else {
+        echo "La cadena $apellidos no es valida";
+    }
+    $correo = $_POST["correo"];
+    $cadena = "e_rne456sto2@educastur.org";
+    $patron = "/^[a-z]{1}([a-z]|[0-9]|_)+@educastur+\.(com|org)$/";
+
+    if (preg_match($patron, $cadena)) {
+        echo "La cadena $cadena es valida";
+    } else {
+        echo "La cadena $cadena no es valida";
+    }
+    $password = $_POST["confirmpasswd"];
+    $passwordcon = $_POST["passwd"];
+    $patron = "/^.{8,n}$/";
+    if (preg_match($patron, $password) && $password == $passwordcon) {
+        echo "La cadena $password es valida";
+    } else {
+        echo "La cadena $password no es valida";
+    }
+    $user = $_POST["usuario"];
+    try {
+        $bd = new PDO($dsn, $usuario, $clave);
+        $sql = "INSERT INTO usuarios(usuario,nombre,apellidos,correo,passw,rol) values (\"$user\",\"$nombre\",\"$apellidos\",\"$correo\",\"$password\",\"1\"); ";
+        if ($bd->query($sql)) {
+            echo "Fila insertada correctamente";
+        } else {
+            echo "La fila no ha podido ser insertada";
+        }
+    } catch (PDOException $e) {
+        echo "Falló la conexión: " . $e->getMessage();
+    }
+}
+?>
+
+</html>
